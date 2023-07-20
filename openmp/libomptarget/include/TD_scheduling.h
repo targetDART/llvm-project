56acb28dcf0af6f18e5be35320502fe3f641f7ed
Adrian Schmitz <a.schmitz@itc.rwth-aachen.de>
2023-07-20 11:54:59 +0200

    class drafts

--- openmp/libomptarget/include/TD_scheduling.h
+++ openmp/libomptarget/include/TD_scheduling.h
@@ -5,6 +5,7 @@
 //TODO: define scheduling interface for TargetDART
 
 #include <cstdint>
+#include <vector>
 #include "omptarget.h"
 #include "device.h"
 #include "TD_common.h"
@@ -27,8 +28,24 @@ class TD_Device_Queue {
     tdrc add_local_task(td_task_t* task);
 
     tdrc poll_task(td_task_t* task);
+
+    TD_Device_Queue();
+};
+
+class TD_Node {
+    private:
+    std::vector<TD_Device_Queue*> td_devices;
+
+    public:
+    double get_total_cost();
+
+    double get_total_tasks();
 };
 
 
+
+tdrc td_init_devices(int64_t num);
+
+tdrc td_finalize_devices();
 
 #endif
